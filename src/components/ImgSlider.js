0d4f0eb912ee31054f81b0a0a1bcc0ac9af3f548
Apdet <deterding1.a@gmail.com>
2021-06-01 17:26:18 -0500

    added react-slider and react-carousel packages to app, added images slider image into the ImgSlider component

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import styled from 'styled-components';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 const ImgSlider = () => {
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+
     return (
-        <Container>
-            ImgSlider
-        </Container>
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="Image Slider" />
+            </Wrap>
+        </Carousel>
     )
 }
 
 export default ImgSlider
 
-const Container = styled.div`
+const Carousel = styled(Slider)`
+
+`;
+
+const Wrap = styled.div`
 
 `;

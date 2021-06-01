02df1e87967f38aa1f419cf2ede808af922659be
Apdet <deterding1.a@gmail.com>
2021-06-01 16:42:51 -0500

    finished header by adding underline transition on hover for the link options, changed background color in index.css, added image to the home section and created image slider component andadded it to the home component

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const ImgSlider = () => {
+    return (
+        <Container>
+            ImgSlider
+        </Container>
+    )
+}
+
+export default ImgSlider
+
+const Container = styled.div`
+
+`;
